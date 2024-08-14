620622b7f9c40e23d7cb334e0cdf8b5da89c4a02
姚新伟 <guihu@yeah.net>
2024-08-14 17:14:49 +0800

    rename

--- UDPInput/src/main.cpp
+++ UDPInput/src/main.cpp
@@ -1,20 +1,23 @@
 #include <fcitx/inputmethodengine.h>
 #include <fcitx/addonfactory.h>
 
-class MyInputMethod : public fcitx::InputMethodEngineV2
+class UDPInput : public fcitx::InputMethodEngineV2
 {
 public:
-    void keyEvent(const fcitx::InputMethodEntry &entry, fcitx::KeyEvent &keyEvent) override{
-         FCITX_UNUSED(entry);
+    void keyEvent(const fcitx::InputMethodEntry &entry, fcitx::KeyEvent &keyEvent) override
+    {
+        FCITX_UNUSED(entry);
         FCITX_INFO() << keyEvent.key() << " isRelease=" << keyEvent.isRelease();
     }
 };
 
-class MyInputMethodFactory : public fcitx::AddonFactory {
-    fcitx::AddonInstance * create(fcitx::AddonManager * manager) override {
+class UDPInputFactory : public fcitx::AddonFactory
+{
+    fcitx::AddonInstance *create(fcitx::AddonManager *manager) override
+    {
         FCITX_UNUSED(manager);
-        return new MyInputMethod();
+        return new UDPInput();
     }
 };
 
-FCITX_ADDON_FACTORY(MyInputMethodFactory);
+FCITX_ADDON_FACTORY(UDPInputFactory);
